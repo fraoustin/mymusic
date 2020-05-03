e09ff814c0efc5173af339329477c843accb0fdc
fraoustin <fraoustin@gmail.com>
2020-05-03 15:59:16 +0200

    <flake> clean

--- mymusic/mymusic.py
+++ mymusic/mymusic.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from .npyscreen import StandardApp, FormBaseNew, wgtextbox, SimpleGrid
 import os
-import sys
 import datetime
 import curses
 from .vlc import State, MediaPlayer
@@ -9,7 +8,7 @@ import click
 
 
 def convertMillis(millis):
-    dte = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc) + datetime.timedelta(seconds=millis/1000)
+    dte = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc) + datetime.timedelta(seconds=millis / 1000)
     return dte.strftime("%H:%M:%S")
 
 
@@ -25,8 +24,11 @@ class M3u():
                 if line.startswith("http://"):
                     self.uri = line.replace('\n', '')
 
+
 class Singleton(type):
+
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
@@ -34,7 +36,7 @@ class Singleton(type):
 
 
 class MyMediaPlayer(metaclass=Singleton):
-    
+
     def __init__(self):
         self.uri = ""
         self.ihm = None
@@ -54,7 +56,7 @@ class MyMediaPlayer(metaclass=Singleton):
     @property
     def author(self):
         return self._author
-    
+
     @property
     def timer(self):
         if self.vlc.get_time() > 0:
@@ -74,7 +76,7 @@ class MyMediaPlayer(metaclass=Singleton):
     @property
     def state(self):
         return self.vlc.get_state()
- 
+
     def change_uri(self, ihm, uri=None):
         self.ihm = ihm
         self.vlc.stop()
@@ -94,26 +96,24 @@ class MyMediaPlayer(metaclass=Singleton):
             self._album = os.path.split(os.path.split(uri)[0])[1]
             self._author = os.path.split(os.path.split(os.path.split(uri)[0])[0])[1]
         self.vlc.play()
-    
-    def play(self):
-        self.vlc.play()
-    
+
     def pause(self):
         self.vlc.pause()
-    
+
     def stop(self):
         self.vlc.stop()
-    
+
     def play(self):
         self.vlc.play()
 
     def sound_more(self):
         vol = self.vlc.audio_get_volume()
-        self.vlc.audio_set_volume(min(vol+5, 100))
+        self.vlc.audio_set_volume(min(vol + 5, 100))
 
     def sound_less(self):
         vol = self.vlc.audio_get_volume()
-        self.vlc.audio_set_volume(max(vol-5, 0))
+        self.vlc.audio_set_volume(max(vol - 5, 0))
+
 
 class MyMusic(StandardApp):
 
@@ -132,7 +132,7 @@ class MainForm(FormBaseNew):
     def __init__(self, path='.', *args, **keywords):
         self.path = os.path.abspath(path)
         self.root = os.path.abspath(path)
-        FormBaseNew.__init__(self, minimum_lines = 8,  minimum_columns = 8,*args, **keywords)
+        FormBaseNew.__init__(self, minimum_lines=8, minimum_columns=8, *args, **keywords)
 
     def create(self):
         y, x = self.useable_space()
@@ -149,7 +149,8 @@ class MainForm(FormBaseNew):
             ord('p'): self.sound_play
         })
         self.add(wgtextbox.TextfieldBase, value='', editable=False)
-        self.infos = [self.add(wgtextbox.TextfieldBase, value='', editable=False, highlight_color='red'),
+        self.infos = [
+            self.add(wgtextbox.TextfieldBase, value='', editable=False, highlight_color='red'),
             self.add(wgtextbox.TextfieldBase, value='', editable=False),
             self.add(wgtextbox.TextfieldBase, value='', editable=False),
             self.add(wgtextbox.TextfieldBase, value='', editable=False)]
@@ -165,11 +166,11 @@ class MainForm(FormBaseNew):
         self.infos[1].update()
         self.infos[2].value = MyMediaPlayer().title
         self.infos[2].update()
-        if MyMediaPlayer().state not in  [State.Stopped, State.NothingSpecial]:
+        if MyMediaPlayer().state not in [State.Stopped, State.NothingSpecial]:
             self.infos[3].value = MyMediaPlayer().timer + " / " + MyMediaPlayer().length + " Volume: " + str(MyMediaPlayer().volume)
         else:
             self.infos[3].value = ""
-        if MyMediaPlayer().state in  [State.Paused]:
+        if MyMediaPlayer().state in [State.Paused]:
             self.infos[3].important = True
         else:
             self.infos[3].important = False
@@ -178,15 +179,14 @@ class MainForm(FormBaseNew):
         self.curses_pad.redrawwin()
         FormBaseNew.refresh(self, *args, **keywords)
 
-
     def _create_gd(self, val=200):
-#       fix to dertmine max width
+        # fix to dertmine max width
         while val > 1:
             try:
                 self.gd = self.add(SimpleGrid, max_height=-5, column_width=val)
                 val = 0
             except Exception:
-                val = val-1
+                val = val - 1
 
     def exit_application(self, *args, **keywords):
         curses.beep()
@@ -211,21 +211,20 @@ class MainForm(FormBaseNew):
     def _update_gd(self, lst):
         self.gd.values = []
         for elt in lst:
-            self.gd.values.append([elt,])
-        self.gd.edit_cell = [0,0]
+            self.gd.values.append([elt, ])
+        self.gd.edit_cell = [0, 0]
 
     def _get_listdir(self, path):
         lstelt = []
         files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
         directorys = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
         if os.path.abspath(path) != self.root:
-            directorys = ['..',] + directorys
+            directorys = ['..', ] + directorys
         for lst in [directorys, files]:
             lst.sort()
             for elt in lst:
                 lstelt.append(elt)
         return lstelt
-
 
     def select_element(self, *args, **keywords):
         selected = self.gd.values[self.gd.edit_cell[0]][0]
@@ -242,6 +241,7 @@ def run(directory):
     """DIRECTORY contains your music"""
     MyApp = MyMusic(directory)
     MyApp.run()
+
 
 if __name__ == '__main__':
     run()
